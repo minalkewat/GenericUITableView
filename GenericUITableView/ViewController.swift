d84bdcd47ee4689034f4989310479e8421cc6022
minal <minalkewat@gmail.com>
2020-04-13 12:21:27 +0530

    Initial Commit

---
+++ GenericUITableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericUITableView
+//
+//  Created by apple on 13/04/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
